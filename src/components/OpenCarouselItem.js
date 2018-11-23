9bc84c724bc117ba5e5644942a74273ad186fa60
Matthew Hartman <matthewhartmanau@gmail.com>
2018-11-23 19:22:21 +1100

    Webpack dev config

--- src/components/OpenCarouselItem.js
+++ src/components/OpenCarouselItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const OpenCarouselItem = ({ width = 200, name, logo, url }) => (
   <a data-testid='open-carousel-item' className='open-carousel-item' href={url}>
     <img alt={name} src={logo} title={name} />
-    <style jsx>{`
+    <style global jsx>{`
       .open-carousel-item {
         width: ${width}px;
         text-align: center;
---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+/*** webpack.config.dev.js ***/
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: path.join(__dirname, "src/demo/src/index.html"),
+  filename: "./index.html"
+});
+module.exports = {
+  entry: path.resolve(__dirname, "src/demo/src/index.js"),
+  module: {
+    rules: [
+    {
+      test: /\.(js|jsx)$/,
+      use: "babel-loader",
+      exclude: /node_modules/
+    },
+    {
+      test: /\.css$/,
+      use: ["style-loader", "css-loader"]
+    }
+    ]
+  },
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    open: true,
+    port: 3000
+  }
+};
